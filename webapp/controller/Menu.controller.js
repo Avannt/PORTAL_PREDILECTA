5131949ea6d08633aa67e48a29ca2c5ff8bc413b
Romulo Pires <romulo.pires@avannt.com.br>
2021-09-09 12:10:03 +0000

    Deploy das alterações do pedido

--- webapp/controller/Menu.controller.js
+++ webapp/controller/Menu.controller.js
@@ -1,9 +1,10 @@
 /*eslint-disable no-console, no-alert */
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"application/controller/BaseController"
-	
-], function (Controller, BaseController) {
+	"application/controller/BaseController",
+	"sap/ui/model/json/JSONModel"
+
+], function (Controller, BaseController, JSONModel) {
 	"use strict";
 
 	return BaseController.extend("application.controller.Menu", {
@@ -15,49 +16,76 @@ sap.ui.define([
 		},
 
 		onRouteMatched: function (oEvent) {
-			
+
 			var that = this;
-			var menu = this.getModelGlobal("modelMenu").getData();
+			var CodRepres = that.getModelGlobal("modelAux").getProperty("/CodRepres");
+			var Cliente = "";
+			var Envio = true;
+			var Count = true;
 			
-			var objGeral = {
-				Principal: [], //posição 01
-				Consultas: [], //posição 02
-				Relatorios: [] //posição 03
-			};
-			
-			for(var i=0; i<menu.length; i++){
-				var posicao = menu[i].Posicao;
-				
-				var vetorGeral = {
-					"id": "",
-					"icon": "",
-					"title": "",
-					"info": "",
-					"visible": "",
-					"number": ""
+			that.oModel = that.getModel();
+
+			new Promise(function (res, rej) {
+
+				that.onBuscarPedidos(Cliente, CodRepres, Envio, res, rej, that, Count);
+
+			}).then(function (result) {
+
+				that.getModelGlobal("modelAux").setProperty("/PedidosPend", result);
+
+				var menu = that.getModelGlobal("modelMenu").getData();
+
+				var objGeral = {
+					Principal: [], //posição 01
+					Consultas: [], //posição 02
+					Relatorios: [] //posição 03
 				};
-				
-				vetorGeral.id = menu[i].IdMenu;
-				vetorGeral.icon = menu[i].Icone;
-				vetorGeral.title = menu[i].DescMenu;
-				vetorGeral.info = menu[i].InfoAdic;
-				vetorGeral.visible = menu[i].Ativo;
-				vetorGeral.number = menu[i].Numero;
-				
-				if(posicao == "01"){ 
-					objGeral.Principal.push(vetorGeral);
-				} else if(posicao == "02"){
-				 	objGeral.Consultas.push(vetorGeral);
-				} else if(posicao == "03"){
-				 	objGeral.Relatorios.push(vetorGeral);
+
+				for (var i = 0; i < menu.length; i++) {
+					var posicao = menu[i].Posicao;
+
+					var vetorGeral = {
+						"id": "",
+						"icon": "",
+						"title": "",
+						"info": "",
+						"visible": "",
+						"number": ""
+					};
+
+					vetorGeral.id = menu[i].IdMenu;
+					vetorGeral.icon = menu[i].Icone;
+					vetorGeral.title = menu[i].DescMenu;
+					vetorGeral.info = menu[i].InfoAdic;
+					vetorGeral.visible = menu[i].Ativo;
+
+					if (menu[i].IdMenu == "P02") { //Envio de pedidos
+
+						vetorGeral.number = that.getModelGlobal("modelAux").getProperty("/PedidosPend");
+					} else {
+
+						vetorGeral.number = menu[i].Numero;
+					}
+
+					if (posicao == "01") {
+						objGeral.Principal.push(vetorGeral);
+					} else if (posicao == "02") {
+						objGeral.Consultas.push(vetorGeral);
+					} else if (posicao == "03") {
+						objGeral.Relatorios.push(vetorGeral);
+					}
 				}
-			}
-			
-			this.getModel("menu").setData(objGeral);
+				that.getModel("menu").setData(objGeral);
+
+			}).catch(function (error) {
+
+				that.onMensagemErroODATA(error);
+			});
+
 		},
 
 		onTile: function (oEvent) {
-			
+
 			switch (oEvent.getSource().data("opcao")) {
 			case "P01":
 				sap.ui.core.UIComponent.getRouterFor(this).navTo("pedido");
